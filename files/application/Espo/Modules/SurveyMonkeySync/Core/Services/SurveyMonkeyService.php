77b41db31e27268983fa8af619cf156707433d13
Telarun-ivan <telarunivan@gmail.com>
2025-01-24 21:55:36 +0200

    survey-monkey-sync ver 1.7.0

--- files/application/Espo/Modules/SurveyMonkeySync/Core/Services/SurveyMonkeyService.php
+++ files/application/Espo/Modules/SurveyMonkeySync/Core/Services/SurveyMonkeyService.php
@@ -39,6 +39,15 @@ class SurveyMonkeyService
         $client = $this->getClient();
         $response = $client->getSurveyResponsesBulk($surveyId,$filters);
 
+        $GLOBALS['log']->error("Response Survey monkey [${$surveyId}]",[
+            'surveyId' => $surveyId,
+            'additionalData' => $filters,
+            'is_error' => $response->isError(),
+            'is_success' => $response->isSuccess(),
+            'error' => $response->getError(),
+            'data' => $response->getData() 
+        ]);
+        
         return $response->getData();
     }
 
@@ -55,7 +64,7 @@ class SurveyMonkeyService
             'type' => 'weblink'
         ]));
 
-        $GLOBALS['log']->info("Response Survey monkey [${$surveyId}]",[
+        $GLOBALS['log']->error("Create Survey collector [${$surveyId}]",[
             'surveyId' => $surveyId,
             'additionalData' => $additionalData,
             'is_error' => $response->isError(),
