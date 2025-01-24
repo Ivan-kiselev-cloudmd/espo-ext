a02a93fbb53a02e22f6cdb5b770da1ba8dbe5fd7
Telarun-ivan <telarunivan@gmail.com>
2025-01-24 21:19:27 +0200

    snapclarity-sync ver 3.2.32

--- files/application/Espo/Modules/SnapclaritySync/Core/Services/ContactImportService.php
+++ files/application/Espo/Modules/SnapclaritySync/Core/Services/ContactImportService.php
@@ -69,6 +69,7 @@ class ContactImportService extends BaseImportService
             'assessmentProgress' => !empty($contactData['assessment_progress'])  ? $contactData['assessment_progress'] : null,
             'assessmentFirstAnswerDate' => !empty($contactData['assessment_first_answer_date']) ? gmdate('Y-m-d H:i:s', strtotime($contactData['assessment_first_answer_date'])) : null,
             'assessmentLastAnswerDate' => !empty($contactData['assessment_last_answer_date'])  ? gmdate('Y-m-d H:i:s', strtotime($contactData['assessment_last_answer_date'])) : null,
+            'requestServiceAs' => !empty($contactData['request_service_as']) ?  $contactData['request_service_as'] : null
         ];
 
         if ($isCreate) {
